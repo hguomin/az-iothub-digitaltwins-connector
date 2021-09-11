5befff625611e5541892fe1ec9f7a9df17195f25
黄国敏 <guomin@GuominMBP.local>
2021-09-11 15:42:10 +0800

    Initial commit

---
+++ src/test/java/com/azure/aziothubdigitaltwinsconnector/AzIothubDigitaltwinsConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.aziothubdigitaltwinsconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzIothubDigitaltwinsConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
