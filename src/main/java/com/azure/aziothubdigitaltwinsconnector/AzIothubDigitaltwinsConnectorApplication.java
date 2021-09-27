f1ee18c26f63cf666b3e7ccc19cdb88543c77828
Guomin Huang <hguomin@outlook.com>
2021-09-27 09:24:52 +0800

    Add Spring Cloud Stream to receive IoT Hub messages

--- src/main/java/com/azure/aziothubdigitaltwinsconnector/AzIothubDigitaltwinsConnectorApplication.java
+++ src/main/java/com/azure/aziothubdigitaltwinsconnector/AzIothubDigitaltwinsConnectorApplication.java
@@ -8,6 +8,7 @@ public class AzIothubDigitaltwinsConnectorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzIothubDigitaltwinsConnectorApplication.class, args);
+
 	}
 
 }
