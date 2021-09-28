32e0f4f5944725aa0a643f627223e53b6cda4f57
Guomin Huang <hguomin@outlook.com>
2021-09-28 11:14:28 +0800

    Test digital twins client authentication

--- src/main/java/com/azure/aziothubdigitaltwinsconnector/AzIoTHubMessageReceiver.java
+++ src/main/java/com/azure/aziothubdigitaltwinsconnector/AzIoTHubMessageReceiver.java
@@ -1,5 +1,8 @@
 package com.azure.aziothubdigitaltwinsconnector;
 
+import com.azure.digitaltwins.core.DigitalTwinsClient;
+import com.azure.digitaltwins.core.DigitalTwinsClientBuilder;
+import com.azure.identity.ClientSecretCredentialBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,4 +16,22 @@ public class AzIoTHubMessageReceiver {
             System.out.println("Azure IoT Hub: " + message);
         };
     }
+/*
+    @Bean
+    DigitalTwinsClient digitalTwinsClient() {
+        String tenantId = "c8006b8a-cb84-4197-bdfa-ddce719bd36d";
+        String clientId = "65379042-8ac0-4ef5-badf-951d09f4f8df";
+        String clientSecret = "VhA7Q~HSonaOfJreZDUSYY4OTKNNc6Zb~XZO2";
+        String adtEndpoint = "https://gmadt.api.sea.digitaltwins.azure.net";
+
+        return new DigitalTwinsClientBuilder()
+                .credential(new ClientSecretCredentialBuilder()
+                        .tenantId(tenantId)
+                        .clientId(clientId)
+                        .clientSecret(clientSecret)
+                        .build())
+                .endpoint(adtEndpoint)
+                .buildClient();
+    }
+ */
 }
--- src/main/java/com/azure/aziothubdigitaltwinsconnector/controllers/IndexController.java
+++ src/main/java/com/azure/aziothubdigitaltwinsconnector/controllers/IndexController.java
@@ -1,15 +1,50 @@
 package com.azure.aziothubdigitaltwinsconnector.controllers;
 
+import com.azure.digitaltwins.core.DigitalTwinsClient;
+import com.azure.digitaltwins.core.DigitalTwinsClientBuilder;
+import com.azure.digitaltwins.core.DigitalTwinsServiceVersion;
+import com.azure.digitaltwins.core.models.DigitalTwinsModelData;
+
+import com.azure.identity.ClientSecretCredentialBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.azure.digitaltwins.core.DigitalTwinsClient;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
-    
+
+    private DigitalTwinsClient digitalTwinsClient; 
+
     @GetMapping
     public String index() {
+        String tenantId = "c8006b8a-cb84-4197-bdfa-ddce719bd36d";
+        String clientId = "65379042-8ac0-4ef5-badf-951d09f4f8df";
+        String clientSecret = "VhA7Q~HSonaOfJreZDUSYY4OTKNNc6Zb~XZO2";
+        String adtEndpoint = "https://gmadt.api.sea.digitaltwins.azure.net";
+
+        digitalTwinsClient = new DigitalTwinsClientBuilder()
+                .credential(new ClientSecretCredentialBuilder()
+                        .tenantId(tenantId)
+                        .clientId(clientId)
+                        .clientSecret(clientSecret)
+                        .build())
+                .endpoint(adtEndpoint)
+                .buildClient();
+        DigitalTwinsServiceVersion adtVersion = digitalTwinsClient.getServiceVersion();
+        System.out.println(adtVersion.getVersion());
+
+        try{
+            DigitalTwinsModelData model = digitalTwinsClient.getModel("dtmi:example:Room;1");
+            System.out.println(model.getModelId());
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+        }
+
         return "index";
     }
 }
